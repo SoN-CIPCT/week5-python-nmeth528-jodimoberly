713719486fca557612341b594bd02c437200e53c
jodimoberly <jlmobe17@gmail.com>
2026-02-08 01:19:46 +0000

    Added week5 file

---
+++ Jodi_Moberly.py
@@ -0,0 +1,38 @@
+#teas =  ["Green", "Chai","Black", "Oolong", "Rooibos", "White"]
+# print(teas)
+
+# ns1 = teas [0:2]
+# print(ns1)
+# ns2 = teas [2:4]
+# print(ns2)
+# ns3 = teas [0:6:5]
+# print(ns3)
+
+# teas = ['Green', 'Chai','Black', 'Oolong', 'Roobios', 'White']
+
+# print("The first two items in the list are:")
+# print(ns1)
+# print("The middle two items in the list are:")
+# print(ns2)
+# print("The first and last items in the list are:") 
+# print(ns3)
+    
+
+foods = ("chicken", "rice", "side salad", "hamburger", "fries")
+print(foods)
+
+for foodItems in foods:
+    print("Menu items: " + foodItems)
+   
+print("Finished")
+
+foods = ("steak", "pasta", "side salad", "hamburger", "fries")
+print(foods)
+
+for foodItems in foods:
+    print("Menu items: " + foodItems)
+   
+print("Finished")
+
+
+
